7d9c13cf9186a84f81def7d70b5baaa01caaf1e9
ponta0929 <ponta.matumoto2@gmail.com>
2019-03-05 23:15:26 +0900

    初期プロジェクトの構成変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ButtonComponent } from './sample/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ButtonComponent]
 })
 export class AppModule { }
